290bf20e44b9b42240b3bf379cdbc697e04ac851
Nicolae <nicgrisicgoldserve@gmail.com>
2017-09-21 00:32:50 +0300

    Face class to process landmarks as normal.

--- Face2FaceApp/Face/Face.swift
+++ Face2FaceApp/Face/Face.swift
@@ -13,5 +13,24 @@ import UIKit
 class Face: NSObject {
     var landmarks : VNFaceLandmarks2D!
     var facepose : CGAffineTransform!
+    var imageFrame : UIImage!
+    var base_boundingbox : CGRect!
+    
+    
+    init(frame: UIImage, observation: VNFaceObservation) {
+        super.init()
+        self.imageFrame = frame
+        self.landmarks = observation.landmarks
+        self.facepose = CGAffineTransform.identity
+        self.base_boundingbox = observation.boundingBox
+        
+    }
+    func convert(_ points: UnsafePointer<vector_float2>, with count: Int) -> [(x: CGFloat, y: CGFloat)] {
+        var convertedPoints = [(x: CGFloat, y: CGFloat)]()
+        for i in 0...count {
+            convertedPoints.append((CGFloat(points[i].x), CGFloat(points[i].y)))
+        }
+        return convertedPoints
+    }
 }
 
