d6c330593eee8e86b706db698b872098af50a4ef
Nicolae <nicgrisicgoldserve@gmail.com>
2017-09-21 23:58:33 +0300

    Get face's feature points

--- Face2FaceApp/Face/Face.swift
+++ Face2FaceApp/Face/Face.swift
@@ -15,7 +15,20 @@ class Face: NSObject {
     var facepose : CGAffineTransform!
     var imageFrame : UIImage!
     var base_boundingbox : CGRect!
+    var base_imageSize : CGSize!
     
+    var faceContour : [CGPoint]!
+    var innerLips : [CGPoint]!
+    var outerLips : [CGPoint]!
+    var leftEye : [CGPoint]!
+    var rightEye : [CGPoint]!
+    var nose : [CGPoint]!
+    var leftEyebrow : [CGPoint]!
+    var rightEyebrow : [CGPoint]!
+    var leftPupil : [CGPoint]!
+    var rightPupil : [CGPoint]!
+    var medianLine : [CGPoint]!
+    var noseCrest : [CGPoint]!
     
     init(frame: UIImage, observation: VNFaceObservation) {
         super.init()
@@ -31,6 +44,47 @@ class Face: NSObject {
             convertedPoints.append((CGFloat(points[i].x), CGFloat(points[i].y)))
         }
         return convertedPoints
+    }
+    func convertPointsForFace(_ landmark: VNFaceLandmarkRegion2D?, imgSize: CGSize?) ->[CGPoint]{
+        return (landmark?.pointsInImage(imageSize: imgSize!))!
+    }
+    func normalize() {
+        if let fcContour = landmarks.faceContour {
+            faceContour = convertPointsForFace(fcContour, imgSize: base_imageSize)
+        }
+        if let inLips = landmarks.innerLips {
+            innerLips = convertPointsForFace(inLips, imgSize: base_imageSize)
+        }
+        if let outLips = landmarks.outerLips {
+            outerLips = convertPointsForFace(outLips, imgSize: base_imageSize)
+        }
+        if let left_eye = landmarks.leftEye {
+            leftEye = convertPointsForFace(left_eye, imgSize: base_imageSize)
+        }
+        if let right_eye = landmarks.rightEye {
+            rightEye = convertPointsForFace(right_eye, imgSize: base_imageSize)
+        }
+        if let nose_p = landmarks.nose {
+            nose = convertPointsForFace(nose_p, imgSize: base_imageSize)
+        }
+        if let leftEye_brow = landmarks.leftEyebrow {
+            leftEyebrow = convertPointsForFace(leftEye_brow, imgSize: base_imageSize)
+        }
+        if let rightEye_brow = landmarks.rightEyebrow {
+            rightEyebrow = convertPointsForFace(rightEye_brow, imgSize: base_imageSize)
+        }
+        if let leftPupil_p = landmarks.leftPupil {
+            leftPupil = convertPointsForFace(leftPupil_p, imgSize: base_imageSize)
+        }
+        if let rightPupil_p = landmarks.rightPupil {
+            rightPupil = convertPointsForFace(rightPupil_p, imgSize: base_imageSize)
+        }
+        if let median_line = landmarks.medianLine {
+            medianLine = convertPointsForFace(median_line, imgSize: base_imageSize)
+        }
+        if let nose_crest = landmarks.noseCrest {
+            noseCrest = convertPointsForFace(nose_crest, imgSize: base_imageSize)
+        }
     }
 }
 
